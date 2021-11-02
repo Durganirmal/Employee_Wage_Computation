7ba01e0b0ec2fd7934ee93785223d29bb75e825d
Durga Nirmal <durganirmal123@gmail.com>
2021-11-02 14:22:11 +0530

    Added :Solution using Switch case Statement

--- src/com/EmployeeWageComputation/Employee_Wage_Computation.java
+++ src/com/EmployeeWageComputation/Employee_Wage_Computation.java
@@ -4,28 +4,25 @@ public class Employee_Wage_Computation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		int is_fulltime_present = 1;
-		int is_parttime_present = 2;
-		int daily_wage;
-		int wage_per_hr = 20;
-		int working_hr;
-		int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
-		if( emp_check == is_fulltime_present)
-		{
-			System.out.println("Employee is full time present");
-			working_hr = 8; 
+	       int is_full_time_present = 1;
+		       int is_part_time_present = 2;
+		       int daily_wage;
+		       int wage_per_hr = 20;
+		       int working_hr=0;
+		       int emp_check = (int)(Math.floor(Math.random() * 10)) % 3;
+		       switch (emp_check) {
+			        case 1:
+				           System.out.println("Employee is full time present");
+				           working_hr = 8;
+				           break;
+			        case 2:
+				           System.out.println("Employee is part time present");
+				           working_hr = 4;
+				           break;
+			        default:
+				System.out.println("Employee is absent");
+				working_hr = 0;
 		}
-		else if( emp_check == is_parttime_present)
-		{
-			System.out.println("Employee is part time present");
-			working_hr = 4;
-		}
-		else
-		{
-			System.out.println("Employee is absent");
-			working_hr = 0;
-		}
-		
 		daily_wage = working_hr * wage_per_hr;
 		System.out.println("Employee daily wage is " + daily_wage);
     }
