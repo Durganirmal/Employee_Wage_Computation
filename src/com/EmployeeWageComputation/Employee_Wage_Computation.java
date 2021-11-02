66bc53269cb43d41791940c58a24e94753ef68e9
Durga Nirmal <durganirmal123@gmail.com>
2021-11-02 13:41:46 +0530

    Added : Employee Present or Absent

--- src/com/EmployeeWageComputation/Employee_Wage_Computation.java
+++ src/com/EmployeeWageComputation/Employee_Wage_Computation.java
@@ -3,8 +3,12 @@ package com.EmployeeWageComputation;
 public class Employee_Wage_Computation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee Wage");
-       
+		System.out.println("Welcome to Employee Wage Computation");
+		double empCheck=Math.floor(Math.random()*10)%2;
+        if(empCheck == 1)
+            System.out.println("Employee is present ");
+        else
+            System.out.println("Employee is absent ");
     }
 
 	}
