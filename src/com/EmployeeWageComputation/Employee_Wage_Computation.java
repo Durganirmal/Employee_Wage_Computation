ffde3dbe9b6f0cb93723d381d8298c7ee0879802
Durga Nirmal <durganirmal123@gmail.com>
2021-11-02 13:35:04 +0530

    Added : Welcome to Employee Wage Computation

--- src/com/EmployeeWageComputation/Employee_Wage_Computation.java
+++ src/com/EmployeeWageComputation/Employee_Wage_Computation.java
@@ -4,11 +4,7 @@ public class Employee_Wage_Computation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to employee Wage");
-        double empCheck=Math.floor(Math.random()*10)%2;
-        if(empCheck == 1)
-            System.out.println("Employee is present ");
-        else
-            System.out.println("Employee is absent ");
+       
     }
 
 	}
